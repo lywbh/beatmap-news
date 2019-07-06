f9b018e54086e474aa56172d0da9b26de7c2957e
yiweiliang1 <yiweiliang1@creditease.cn>
2019-07-06 13:27:10 +0800

    完成音乐预览播放功能

--- popup.js
+++ popup.js
@@ -2,57 +2,72 @@ $.ajax({
     url: 'https://osu.ppy.sh/beatmapsets/search',
     type: 'get',
     success: function (res) {
-        console.log(res);
-        let itemLi = $("#beatmapList");
-        for (let i in res.beatmapsets) {
-            let item = $("<li></li>");
-            item.append(buildItem(res.beatmapsets[i]));
-            itemLi.append(item);
-            let backgroundUrl = res.beatmapsets[i].covers.card;
-            item.find(".panel_up").css("background-image","url(" + backgroundUrl + ")");
-            item.bind("click", function () {
-                let mapList = res.beatmapsets[i].beatmaps;
-                chrome.tabs.create({
-                    'url': mapList[mapList.length - 1].url
-                });
-            });
-            let playButton = item.find(".title_play");
-            playButton.bind("click", function () {
-                // TODO 把整个列表的播放按钮都变回播放
-                // TODO 把当前按钮至为暂停
-                // TODO 播放音乐
-                if (playButton.attr("src") === "play.png") {
-                    playButton.attr("src", "pause.png");
-                } else {
-                    playButton.attr("src", "play.png");
-                }
-                return false;
-            });
-        }
+        createList(res);
+        bindPlay();
     }
 });
 
+function createList(res) {
+    let itemUl = $("#beatmapList");
+    for (let i in res.beatmapsets) {
+        let item = $("<li></li>");
+        item.append(buildItem(res.beatmapsets[i]));
+        itemUl.append(item);
+        let backgroundUrl = res.beatmapsets[i].covers.card;
+        item.find(".panel_up").css("background-image", "url(" + backgroundUrl + ")");
+        item.bind("click", function () {
+            let mapList = res.beatmapsets[i].beatmaps;
+            chrome.tabs.create({
+                'url': mapList[mapList.length - 1].url
+            });
+        });
+    }
+}
+
+function bindPlay() {
+    let itemUl = $("#beatmapList");
+    let audio = $("#previewAudio");
+    let playButton = itemUl.find(".title_play");
+    playButton.bind("click", function () {
+        try {
+            audio[0].pause();
+            if ($(this).attr("src") === "play.png") {
+                audio.attr("src", $(this).attr("preview"));
+                audio[0].play();
+                playButton.attr("src", "play.png");
+                $(this).attr("src", "pause.png");
+            } else if ($(this).attr("src") === "pause.png") {
+                $(this).attr("src", "play.png");
+            }
+            return false;
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
+    });
+}
+
 function buildItem(beatmap) {
-   return $('<div class="panel">' +
-        '    <div class="panel_up">' +
-        '        <div class="status">' +
-        '            <span class="rankStatus">' + beatmap.status + '</span>' +
-        '            <span class="playStatus">' +
+    return $('<div class="panel">' +
+        '        <div id="preview_url" class="hidden">' + beatmap.preview_url + '</div>' +
+        '        <div class="panel_up">' +
+        '            <div class="status">' +
+        '                <span class="rank_status">' + beatmap.status + '</span>' +
+        '                <span class="play_status">' +
         '                <span>' + beatmap.play_count + ' plays</span><br>' +
         '                <span>' + beatmap.favourite_count + ' loves</span>' +
-        '            </span>' +
-        '        </div>' +
-        '        <div class="title">' +
-        '            <div class="title_info">' +
-        '                <h4>' + beatmap.title + '</h4>' +
-        '                <h5>' + beatmap.artist + '</h5>' +
         '            </div>' +
-        '            <img class="title_play" src="play.png" alt="">' +
+        '            <div class="title">' +
+        '                <div class="title_info">' +
+        '                    <h4>' + beatmap.title + '</h4>' +
+        '                    <h5>' + beatmap.artist + '</h5>' +
+        '                </div>' +
+        '                <img class="title_play" src="play.png" alt="" preview="http:' + beatmap.preview_url + '">' +
+        '            </div>' +
         '        </div>' +
-        '    </div>' +
-        '    <div class="panel_down">' +
-        '        <span class="mapper">' + beatmap.creator + '</span>' +
-        '        <span class="source">' + beatmap.source + '</span>' +
-        '    </div>' +
-        '</div>');
+        '        <div class="panel_down">' +
+        '            <span class="mapper">' + beatmap.creator + '</span>' +
+        '            <span class="source">' + beatmap.source + '</span>' +
+        '        </div>' +
+        '    </div>');
 }
