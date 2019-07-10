17cffefc5dfde81349d243b6435ef792899141a1
yiweiliang1 <yiweiliang1@creditease.cn>
2019-07-10 17:06:15 +0800

    下载放在当前页面进行，防止插件页被关闭；优化部分代码

--- src/js/popup.js
+++ src/js/popup.js
@@ -22,8 +22,7 @@ moreButton.bind("click", function () {
 });
 
 function initData(cursor, q) {
-    moreButton.hide();
-    loading.show();
+    switchMore();
     let param = '';
     if (q) {
         param += '&q=' + q;
@@ -37,68 +36,91 @@ function initData(cursor, q) {
         url: baseUrl + "/beatmapsets/search?" + param.substring(1),
         type: 'get',
         success: function (res) {
-            if (q && res.cursor && !res.cursor._score) {
-                searchData.val("");
-                searchData.attr("placeholder", "请先前往官网登录");
-                searchButton.addClass("warn");
-                setTimeout(function () {
-                    searchData.attr("placeholder", "请输入要检索的谱面");
-                    searchButton.removeClass("warn");
-                }, 2000);
-            }
+            checkLogin(q, res.cursor);
             appendList(res);
             if (res.cursor) {
                 cursorStore = res.cursor;
             }
-            loading.hide();
-            moreButton.show();
+            switchMore();
         },
         error: function (e) {
             console.log(e);
         }
     });
+
+    function checkLogin(q, cursor) {
+        if (q && cursor && !cursor._score) {
+            searchData.val("");
+            searchData.attr("placeholder", "请先前往官网登录");
+            searchButton.addClass("warn");
+            setTimeout(function () {
+                searchData.attr("placeholder", "请输入要检索的谱面");
+                searchButton.removeClass("warn");
+            }, 2000);
+        }
+    }
+
+    function switchMore() {
+        if (moreButton.is(":visible")) {
+            moreButton.hide();
+            loading.show();
+        } else {
+            loading.hide();
+            moreButton.show();
+        }
+    }
 }
 
 function appendList(res) {
     for (let i in res.beatmapsets) {
         let mapInfo = res.beatmapsets[i];
-        let item = $("<li></li>");
-        item.append(buildItem(mapInfo));
+        let item = $("<li></li>").append(buildItem(mapInfo));
         mapList.append(item);
         let imgPanel = item.find(".panel_up");
         imgPanel.css("background-image", "url(" + mapInfo.covers.card + ")");
         imgPanel.bind("click", function () {
             let mapList = mapInfo.beatmaps;
-            chrome.tabs.create({"url": mapList[mapList.length - 1].url});
-        });
-        item.find(".download").bind("click", function () {
-            chrome.tabs.create({"url": baseUrl + "/beatmapsets/" + mapInfo.id + "/download"});
+            chrome.tabs.create({url: mapList[mapList.length - 1].url});
         });
         item.find(".mapper").bind("click", function () {
-            chrome.tabs.create({"url": baseUrl + "/users/" + mapInfo.user_id});
+            chrome.tabs.create({url: baseUrl + "/users/" + mapInfo.user_id});
+        });
+        bindPlay(item.find(".title_play"), mapInfo);
+        bindDownload(item.find(".download"), mapInfo);
+    }
+
+    function bindDownload(button, mapInfo) {
+        button.one("click", function () {
+            button.css("box-shadow", "inset 0 0 4px red");
+            chrome.downloads.download({
+                url: baseUrl + "/beatmapsets/" + mapInfo.id + "/download",
+                filename: mapInfo.id + " - " + mapInfo.title + ".osz",
+                saveAs: true
+            }, function () {
+                button.css("box-shadow", "");
+                bindDownload(button, mapInfo);
+            });
         });
     }
-    bindPlay();
-}
 
-function bindPlay() {
-    let playButton = mapList.find(".title_play");
-    playButton.unbind("click").bind("click", function () {
-        try {
-            audio[0].pause();
-            if ($(this).text() === "▲") {
-                audio.attr("src", $(this).attr("preview"));
-                audio[0].play();
-                playButton.text("▲");
-                $(this).text("〓");
-            } else if ($(this).text() === "〓") {
-                $(this).text("▲");
+    function bindPlay(button, mapInfo) {
+        button.bind("click", function () {
+            try {
+                audio[0].pause();
+                if ($(this).text() === "▲") {
+                    audio.attr("src", "https:" + mapInfo.preview_url);
+                    audio[0].play();
+                    mapList.find(".title_play").text("▲");
+                    $(this).text("〓");
+                } else if ($(this).text() === "〓") {
+                    $(this).text("▲");
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
-        }
-        return false;
-    });
+            return false;
+        });
+    }
 }
 
 function buildItem(mapInfo) {
@@ -116,7 +138,7 @@ function buildItem(mapInfo) {
         '                        <h4>' + mapInfo.title + '</h4>' +
         '                        <h5>' + mapInfo.artist + '</h5>' +
         '                    </div>' +
-        '                    <a class="title_play" preview="http:' + mapInfo.preview_url + '">▲</a>' +
+        '                    <a class="title_play">▲</a>' +
         '                </div>' +
         '            </div>' +
         '            <div class="panel_down">' +
